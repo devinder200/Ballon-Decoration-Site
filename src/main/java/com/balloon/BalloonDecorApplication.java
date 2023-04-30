4c05f31d5ba28bcdb00abf979f48aaf65e4e5940
Devinder <devinderwadhwa20@gmail.com>
2023-04-30 15:18:39 +0530

    first initial commit

---
+++ src/main/java/com/balloon/BalloonDecorApplication.java
@@ -0,0 +1,13 @@
+package com.balloon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalloonDecorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalloonDecorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/balloon/model/Customer.java
@@ -0,0 +1,6 @@
+package com.balloon.model;
+
+public class Customer {
+
+	
+}
